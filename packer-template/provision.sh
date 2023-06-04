6156f05ce836860e030364dedab2eb1f61630ab5
sergioaten <sergio_10_96@hotmail.es>
2023-06-04 20:11:20 +0200

    script modified

--- packer-template/provision.sh
+++ packer-template/provision.sh
@@ -4,6 +4,6 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent softwa
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update -y
-sudo apt-get install python3-pip default-jdk docker.io -y
-sudo useradd jenkins
+sudo apt-get install -y python3-pip default-jdk docker.io
+sudo adduser --disabled-password --gecos "" jenkins
 sudo usermod -aG docker jenkins
