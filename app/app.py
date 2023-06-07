e90e57bf860c7e6bec896eb06a0364cfe618e0e4
sergioaten <sergio_10_96@hotmail.es>
2023-06-07 22:45:47 +0200

    app jenkinsfiler adding a msg in python json return

--- app/app.py
+++ app/app.py
@@ -62,6 +62,7 @@ def get_data():
         'architecture' : platform.machine(),
         'python_version' : platform.python_version(),
         'node': platform.node(),
+        'msg': "Mensaje de prueba para testear la integración continua :)"
     }
 
     # Convert the data to JSON format
