ce48175c52b93d47809f87ba008d97f39f30c4b4
sergioaten <sergio_10_96@hotmail.es>
2023-06-08 23:39:46 +0200

    spliting repos to -> GCP.cicd-project.app GCP.cicd-project.terraform GCP.cicd-project.packer

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "objects_to_upload" {
-  default = {
-    bitnami-script = {
-      remote_path = "scripts/bitnami-startup.sh"
-      local_path  = "scripts/bitnami-startup.sh"
-    }
-  }
-}
-
